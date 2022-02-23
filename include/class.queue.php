ff45e7f00ec9f92279fe01c34c7fa6ea8fabe020
Payal <pkul1998@gmail.com>
2022-02-23 16:05:53 +0530

    Additional changes made for Adding Project manager for Jhansi

--- include/class.queue.php
+++ include/class.queue.php
@@ -567,6 +567,18 @@ class CustomQueue extends VerySimpleModel {
                         $pieces['user__cdata__projectlinked+!includes']->ht['choices']=$temp_arx;
                         //error_log(print_r($temp_arx,TRUE));
                 }
+                elseif($thisstaff->ht['lastname']=="Anand Yadav")
+                {
+                    $temp_arx=$pieces['user__cdata__projectlinked+!includes']->ht['choices'];
+                        foreach ($temp_arx as $indexx=>$item) {
+                            if($item=="Jhansi Smart City")
+                                continue;
+                            else
+                                unset($temp_arx[$indexx]);
+                        }
+                        $pieces['user__cdata__projectlinked+!includes']->ht['choices']=$temp_arx;
+                        //error_log(print_r($temp_arx,TRUE));
+                }
               }
            }
         }
